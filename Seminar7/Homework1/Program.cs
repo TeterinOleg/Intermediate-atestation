36fc666e0a6be8f054296c90199fa28ebb5df649
Oleg Teterin <teterinolegv@gmail.com>
2024-03-26 18:05:09 +0300

    ToInt32

---
+++ Seminar7/Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+
+        Console.Write("Введите число для проверки: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (CheckDivisibility(number))
+        {
+            Console.WriteLine($"{number} кратно одновременно 7 и 23.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не кратно одновременно 7 и 23.");
+        }
+    
+
+    static bool CheckDivisibility(int num)
+    {
+        if (num % 7 == 0 && num % 23 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
