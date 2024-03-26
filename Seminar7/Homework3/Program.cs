8ca8300cef4b2c566216693872750fb49efaf18a
Oleg Teterin <teterinolegv@gmail.com>
2024-03-26 18:47:37 +0300

    PrintArrayReverse

---
+++ Seminar7/Homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = { 3, 2, 8, 4, 1, 6, 9 }; // Произвольный массив
+PrintArrayReverse(array, array.Length - 1);
+
+// Рекурсивный метод для вывода элементов массива, начиная с конца
+static void PrintArrayReverse(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " "); // Выводим текущий элемент
+        PrintArrayReverse(array, index - 1); // Рекурсивный вызов для предыдущего элемента
+    }
+}
