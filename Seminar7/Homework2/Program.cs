ed4e664aeda2b4ff1ce6552f054f0015d111b822
Oleg Teterin <teterinolegv@gmail.com>
2024-03-26 18:41:04 +0300

    PrintNaturalNumbersInRange

---
+++ Seminar7/Homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿int M = 4; // Задаем начальное значение промежутка
+int N = 34; // Задаем конечное значение промежутка
+
+PrintNaturalNumbersInRange(M, N);
+
+// Рекурсивный метод для вывода всех натуральных чисел в промежутке от M до N
+static void PrintNaturalNumbersInRange(int M, int N)
+{
+    if (M <= N)
+    {
+        Console.Write(M + " "); // Выводим текущее число
+        PrintNaturalNumbersInRange(M + 1, N); // Рекурсивный вызов для следующего числа
+    }
+}
